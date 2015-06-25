658c58cca67b2f5860a792e7d2f558109a14feb9
fabricioazevedo <fabricioazevedo87@gmail.com>
2015-06-25 12:34:39 -0700

    mysql connection and other

---
+++ first.py
@@ -0,0 +1,14 @@
+'''
+Created on 25 de jun de 2015
+@author: Fabricio
+'''
+
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+    y = 5
+    return y*a
+  
+print add(1,2)
+print addFixedValue(2) 
---
+++ mysqlconnection.py
@@ -0,0 +1,23 @@
+'''
+Created on 25 de jun de 2015
+
+@author: Fabricio
+'''
+
+import mysql.connector
+from mysql.connector import errorcode
+
+try:
+
+    cnx = mysql.connector.connect(user='root', password='*******',
+                              host='127.0.0.1',
+                              database='py_db')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  cnx.close()
